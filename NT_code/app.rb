484ed656e6aafa2570ec2f2aa149b0f50407fc4e
CassyLee <cassyljs@126.com>
2015-10-14 20:48:36 -0400

    add model

---
+++ NT_code/app.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/person'        #Model class
+require './models/event'
+require './models/regist'
