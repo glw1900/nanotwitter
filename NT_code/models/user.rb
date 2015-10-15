079c4cabd83508649426a882dc09b36ecc71a8cc
CassyLee <cassyljs@126.com>
2015-10-14 21:23:57 -0400

    users.rb
    user has_many

--- NT_code/models/user.rb
+++ NT_code/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
   has_many :messages
   has_many :users, :through => :messages
   has_many :follows
-  has_many :users
+  has_many :users, :through => :follows
+  has_many :favorites
+  has_many :tweets, :through => :favorites
+  has_many :c_mentions
+  has_many :comments, :through => :c_mentions
+  has_many :t_mentions
+  has_many :tweets, :throuh => :t_mentons
 end
