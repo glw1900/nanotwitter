de5073de2ebc1b827c16752e9c2bf0fec649e7a1
glw1900 <guoliangwei1900@gmail.com>
2015-11-01 22:18:49 -0500

    This is a good version

--- NT_code/app.rb
+++ NT_code/app.rb
@@ -13,38 +13,96 @@ require './models/user'
 require './models/tweet'
 require './process'
 enable :sessions
+set :public_folder, File.dirname(__FILE__)+'/bootstrap-3.3.5-dist'
 require 'pry-byebug'
 
+# get '/' do
+#   @logged_in = false #0 when no user log in
+#   if session["username"] != nil
+#     @logged_in = true
+#     @username = session["username"]
+#   end
+#   @parameters = Hash.new
+#   @parameters["unlogged_twitter_list"] = first_50_tweets_list
+#   erb :home
+# end
+
 get '/' do
-  @logged_in = false #0 when no user log in
-  if session["username"] != nil
-    @logged_in = true
-    redirect '/timeline'
+  @parameters = Hash.new
+  @parameters["unlogged_twitter_list"] = first_50_tweets_lst
+  erb :home
+end
+
+get '/timeline' do
+  username = session[:username]
+  @uname = username
+  user_id = User.find_by(username: username).id
+  if !session["username"].nil?
+    @tweet_list = Tweet.where(user_id: user_id)
+    erb :timeline
   end
-  erb :homepage
 end
 
 get '/signup' do
-  erb :sign_up
+  erb :regist
 end
 
-post '/signout' do
-  if session["username"] == nil
-    'already sign out'
+get '/signin' do
+  erb :sign_in
+end
+
+get '/logout' do
+  erb :home
+end
+
+#go to the person page of some one
+get '/users/:username' do
+  if User.find_by(username: params[:username]).nil?
+    @mode = -1
   else
-    @username = session["username"]
-    session["username"] = nil
+    @follow_from_id = User.find_by(username: session[:username]).id
+    @follow_to_id = User.find_by(username: params[:username]).id
+    @pageuser = params[:username]
+    if @follow_from_id == @follow_to_id
+      #user is checiing his or her own page
+      @mode = 0
+    elsif Follow.where(follower_id: @follow_from_id, followee_id: @follow_to_id).size > 0
+      #alreadt followed, show a button to unfollow
+      @mode = 1
+    else
+      #show a follow button
+      @mode = 2
+    end
   end
-  redirect "/"
-  erb :sign_out
+  @uname = params[:username]
+  user_id = User.find_by(username: @uname).id
+  @tweet_list = Tweet.where(user_id: user_id)
+  erb :personpage2
+end
+
+get '/follow/:username' do
+  @username = params[:username]
+  #get the id of the current pageuser
+  user_id = User.find_by(username: @username).id
+  follow_ids = Follow.where(follower_id: user_id)
+  # binding.pry
+  @follow_users = []
+  follow_ids.each do |follow|
+    fname = User.find_by(id: follow.followee_id)
+    @follow_users << fname
+  end
+  erb :followlist
 end
 
 post '/users/submit_regis' do
-  @user = User.new(params[:user])
-  username = @user.username
-  if @user.save
-    redirect '/'
+  u = check(params[:user])
+  if u
+    @user = User.new(u)
+    if @user.save
+      redirect '/signin'
+    end
   end
+  "Password not match"
 end
 
 post "/users/submit" do
@@ -72,28 +130,6 @@ post "/users/submit_twitter" do
   end
 end
 
-get '/timeline' do
-  username = session[:username]
-  @uname = username
-  user_id = User.find_by(username: username).id
-  if !session["username"].nil?
-    @tweet_list = Tweet.where(user_id: user_id)
-    erb :timeline
-  end
-end
-
-post '/users/follow' do
-  @follow = Follow.new()
-  @follow.follower_id = User.find_by(username: params[:follow][:follower]).id
-  @follow.followee_id = User.find_by(username: params[:follow][:followee]).id
-  if @follow.save
-    username = params[:follow][:follower]
-    redirect '/follow/' + username
-  else
-    "wrong when creating follow"
-  end
-end
-
 post '/followings/create' do
   @follow = Follow.new()
   @follow.follower_id = params[:follow_from_id]
@@ -107,55 +143,21 @@ post '/followings/create' do
   end
 end
 
-post '/followings/create' do
-  @follow = Follow.new()
-  @follow.follower_id = params[:follow_from_id]
-  @follow.followee_id = params[:follow_to_id]
-  # binding.pry
-  if @follow.save
-    username = User.find_by(id: params[:follow_from_id]).username
-    redirect '/follow/' + username
+post '/followings/destroy' do
+  follow = Follow.find_by(followee_id: params[:follow_to_id], follower_id: params[:follow_from_id])
+  follow_to_name = User.find_by(id: params[:follow_to_id]).username
+  if follow != nil
+    follow.destroy
+    'unfollow success' + follow_to_name
   else
-    "error when creating follow"
+    "ooooooooooooops, you've never been a fan, are you?"
   end
+  redirect '/users/' + follow_to_name
 end
 
-#go to the person page of some one
-get '/users/:username' do
-  if User.find_by(username: params[:username]).nil?
-    @mode = -1
-  else
-    @follow_from_id = User.find_by(username: session[:username]).id
-    @follow_to_id = User.find_by(username: params[:username]).id
-    @pageuser = params[:username]
-    if @follow_from_id == @follow_to_id
-      #user is checiing his or her own page
-      @mode = 0
-    elsif Follow.where(follower_id: @follow_from_id, followee_id: @follow_to_id).size > 0
-      #alreadt followed, show a button to unfollow
-      @mode = 1
-    else
-      #show a follow button
-      @mode = 2
-    end
+post '/delete/user/:username' do
+  user = User.find_by(username: params[:username])
+  if user != nil
+    user.destroy
   end
-  @uname = params[:username]
-  user_id = User.find_by(username: @uname).id
-  @tweet_list = Tweet.where(user_id: user_id)
-  erb :personpage
-end
-
-
-get '/follow/:username' do
-  @username = params[:username]
-  #get the id of the current pageuser
-  user_id = User.find_by(username: @username).id
-  follow_ids = Follow.where(follower_id: user_id)
-  # binding.pry
-  @follow_users = []
-  follow_ids.each do |follow|
-    fname = User.find_by(id: follow.followee_id)
-    @follow_users << fname
-  end
-  erb :followlist
 end
