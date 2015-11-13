8ccfa4dc2fee00b833d6d54a52990a630a8ae675
TIANZHI_CHEN <burgesschen1990@gmail.com>
2015-11-13 17:01:15 -0500

    fix test interface

--- controller/test_interface.rb
+++ controller/test_interface.rb
@@ -4,24 +4,23 @@ require 'bulk-insert-active-record'
 
 get '/test/tweets/:num' do
     testuser_id = User.find_by(username: "testuser").id
-	values_tweet = Array.new
-	columns_tweet = [:user_id, :content]
-    params['num'].to_i.times do |i|
-        Tweet.create(user_id: testuser_id, content: Faker::Lorem.sentence)
+    i = 0
+    num = params['num'].to_i
+    while(i < num) do
+        Tweet.create(user_id: testuser_id, content: "a fake tweet")
+        i = i + 1
     end
+    "test fweets"
 end
 
 
 get '/test/reset' do
-  # $testuser_id = User.find_by(username: test_user_name)
 	testuser = User.find_by(username: "testuser")
 	if testuser != nil
-    # $testuser_id = testuser.id
     	testuser_id = testuser.id
 		Tweet.destroy_all("user_id = " + testuser_id.to_s)
 		Follow.destroy_all("follower_id = " + testuser_id.to_s)
 		Follow.destroy_all("followee_id = " + testuser_id.to_s)
-		# User.create(username: "testuser", email: Faker::Internet.email, password: "1234", profile: nil) 
 	else
 		User.create(username: "testuser", email: Faker::Internet.email, password: "1234", profile: nil) 
 	end
@@ -32,7 +31,7 @@ end
 get '/test/seed/:num' do
     # testuser_id = User.find_by(username: test_user_name)
 	params[:num].to_i.times do |i|
-    	User.create(username: "test_username#{i}", email: Faker::Internet.email,
+    	User.create(username: "test_username#{i}", email: "fake@email.com",
     	password: "1234", profile: nil)
 	end
 	params[:num] +' users created'
