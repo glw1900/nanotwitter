edd2d9c892f4a75e8adec943af19105520442c7a
glw1900 <guoliangwei1900@gmail.com>
2015-11-13 17:55:57 -0500

    test

--- controller/test_interface.rb
+++ controller/test_interface.rb
@@ -29,7 +29,7 @@ get '/test/reset' do
   if User.find_by(username: "testuser").nil?
     "dame it"
   else
-    'reset finished, testuser created' + User.find_by(username: "testuser").username
+    'reset finished, testuser created' + User.find_by(username: "testuser").id
   end  
 end
 
