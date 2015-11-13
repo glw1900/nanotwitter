a84e107fd74af2b67a1ece7327540dafb337c301
glw1900 <guoliangwei1900@gmail.com>
2015-11-13 17:34:57 -0500

    test

--- controller/test_interface.rb
+++ controller/test_interface.rb
@@ -6,9 +6,9 @@ get '/test/tweets/:num' do
     testuser_id = User.find_by(username: "testuser").id
 	values_tweet = Array.new
 	columns_tweet = [:user_id, :content]
-    params['num'].to_i.times do |i|
-        Tweet.create(user_id: testuser_id, content: Faker::Lorem.sentence)
-    end
+    # params['num'].to_i.times do |i|
+    #     Tweet.create(user_id: testuser_id, content: Faker::Lorem.sentence)
+    # end
 end
 
 
