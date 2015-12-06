6b6b5eba326e855ab2d11ab1121ddc90e520a5b3
TIANZHI_CHEN <burgesschen1990@gmail.com>
2015-12-06 00:34:22 -0500

    debug

--- client_code/api_test_2.rb
+++ client_code/api_test_2.rb
@@ -3,15 +3,16 @@ ENV['RACK_ENV'] = 'development'
 require 'minitest/autorun'
 require_relative 'lib'
 require 'http'
+require_relative 'apitestcode'
 
-$url = "http://protected-refuge-6584.herokuapp.com"
+$url = "http://hugetwitter.herokuapp.com"
 
 
 
-$tweet_id = 100
-$user_id
+$tweet_id = 20
+$user_id = 2
 $recent_n_tweets = 100
-$recent_n_user_tweets
+$recent_user_id = 3
 $recent_user_id = 12
 
 # followship
@@ -25,29 +26,34 @@ describe "a bunch of crazy stuff" do
     response = get_tweet_by_id($tweet_id)
     response.wont_be nil
   end
-
-  it "get user info by id" do
-    response = get_user_info($user_id)
-    response.wont_be nil
-  end
-
-  it "get recent tweets" do
-    response = recent_50_tweets($recent_n_tweets)
-    response.wont_be nil
-  end
-
-  it "get recent user n tweets" do
-    response = recent_n_user_tweets($recent_user_id)
-    response.wont_be nil
-  end
-
-  it "build a friendship" do
-    response = make_follow($name_a, $name_b)
-    response.wont_be nil
-
-    response = destroy_follow($name_a, $name_b)
-    response.wont_be nil
-  end
+  #
+  # it "get user info by id" do
+  #   response = get_user_info($user_id)
+  #   response.wont_be nil
+  # end
+  #
+  # it "get recent tweets" do
+  #   response = recent_50_tweets()
+  #   response.wont_be nil
+  # end
+  #
+  # it "get recent user n tweets" do
+  #   response = recent_n_user_tweets($recent_user_id)
+  #   response.wont_be nil
+  # end
+  #
+  # it "build a friendship" do
+  #   response = make_follow($name_a, $name_b)
+  #   response.wont_be nil
+  #
+  #   response = destroy_follow($name_a, $name_b)
+  #   response.wont_be nil
+  # end
+  #
+  # it "test user timeline" do
+  #   response = get_user_timeline($user_name)
+  #   return response
+  # end
 
 
 end
--- client_code/apitestcode.rb
+++ client_code/apitestcode.rb
@@ -1,52 +1,50 @@
-class FantasticFour
-  attr_accessor :url, :logged_username, :status
-  def get_tweet_by_id(tweet_id)
-    response = JSON.parse(HTTP.get($url + "/api/v1/tweets/" + tweet_id.to_s).body)
-    return response
-  end
 
-  def get_user_info(user_id)
-    response = JSON.parse(HTTP.get($url + "/api/v1/users/" + user_id.to_s).body)
-    return response
-  end
+def get_tweet_by_id(tweet_id)
+  response = JSON.parse(HTTP.get($url + "/api/v1/tweets/" + tweet_id.to_s).body)
+  return response
+end
 
-  def recent_50_tweets()
-    response = JSON.parse(HTTP.get($url + "/api/v1/tweets/recent/").body)
-    return response
-  end
+def get_user_info(user_id)
+  response = JSON.parse(HTTP.get($url + "/api/v1/users/" + user_id.to_s).body)
+  return response
+end
 
-  def recent_n_user_tweets(user_id)
-    response = JSON.parse(HTTP.get($url + "/api/v1/users/" + user_id.to_s + "/tweets").body)
-    return response
-  end
+def recent_50_tweets()
+  response = JSON.parse(HTTP.get($url + "/api/v1/tweets/recent/").body)
+  return response
+end
 
-  def make_follow(username_1, username_2)
-    response = JSON.parse(HTTP.post($url + "/api/v1/create/follow", :params =>
-    {
-      "follower_name" => username_1,
-      "followee_name" => username_2 }).body)
-      #status
-      if(response["status"] == "true")
-        return true
-      else
-        return false
-      end
-  end
+def recent_n_user_tweets(user_id)
+  response = JSON.parse(HTTP.get($url + "/api/v1/users/" + user_id.to_s + "/tweets").body)
+  return response
+end
 
-  def destroy_follow(username_1, username_2)
-    response = JSON.parse(HTTP.post($url + "/api/v1/delete/follow", :params =>
-    {
-     "follower_name" => username_1,
-     "followee_name" => username_2 }))
-     if(response["status"] == "true")
-       return true
-     else
-       return false
-     end
-  end
+def make_follow(username_1, username_2)
+  response = JSON.parse(HTTP.post($url + "/api/v1/create/follow", :params =>
+  {
+    "follower_name" => username_1,
+    "followee_name" => username_2 }).body)
+    #status
+    if(response["status"] == "true")
+      return true
+    else
+      return false
+    end
+end
 
-  def get_user_timeline(user_name)
-    response = JSON.parse(HTTP.get($url + "/api/v1/timeline/" + user_name).body)
-    return response
-  end
+def destroy_follow(username_1, username_2)
+  response = JSON.parse(HTTP.post($url + "/api/v1/delete/follow", :params =>
+  {
+   "follower_name" => username_1,
+   "followee_name" => username_2 }).body)
+   if(response["status"] == "true")
+     return true
+   else
+     return false
+   end
+end
+
+def get_user_timeline(user_name)
+  response = JSON.parse(HTTP.get($url + "/api/v1/timeline/" + user_name).body)
+  return response
 end
