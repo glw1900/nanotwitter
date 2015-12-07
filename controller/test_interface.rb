8e9d13f7ad0621d284d91802883c81d1f55b2ef8
glw1900 <guoliangwei1900@gmail.com>
2015-12-07 18:29:46 -0500

    s

--- controller/test_interface.rb
+++ controller/test_interface.rb
@@ -50,7 +50,7 @@ end
 get '/test/seed/:num' do
   # testuser_id = User.find_by(username: test_user_name)
 	params[:num].to_i.times do |i|
-    	# User.create(username: "test_username#{i}", email: "fake@email.com", password: "1234", profile: nil)
+    	User.create(username: "test_username#{i}", email: "fake@email.com", password: "1234", profile: nil)
 	end
 	params[:num] +' users created'
 end
