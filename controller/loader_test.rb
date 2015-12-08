3a8f10122ab0d660515cdc5648587fa3ccba5747
glw1900 <guoliangwei1900@gmail.com>
2015-12-08 02:27:08 -0500

    asdf

--- controller/loader_test.rb
+++ controller/loader_test.rb
@@ -31,9 +31,7 @@ post '/user/testuser/tweet' do
     tweet = {}
     tweet["content"] = Faker::Bitcoin.address
     tweet["media_url"] = "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg"
-    tweet["retweet_id"] = nil
     tweet["user_id"] = logged_id
-    tweet["pub_time"] = nil
     newest_50_queue = "newest50queue"
     @new_tweet = Tweet.new(tweet)
     if @new_tweet.save!
