2e95337e7ee4a9aa06031816d51febeeeb9b397c
glw1900 <guoliangwei1900@gmail.com>
2015-12-07 22:53:07 -0500

    sdf

--- controller/users.rb
+++ controller/users.rb
@@ -4,14 +4,16 @@ $redis = Redis.new(:url => "redis://redistogo:6089d2a4552e7700e65eebca0fdbca63@p
 get '/users/:username' do
   #add follower_id and followee_id to @parameters
   if params[:username] == "testuser"
-    redirect '/users/testuser'
+    "fuck"
+    # redirect '/users/testuser'
   else
     logged_username = session[:username]
     logged_id = User.find_by(username: logged_username).id
     look_at_username = params[:username]
     look_at_user_id = User.find_by(username: look_at_username).id
     @parameters = user_a_look_at_user_b_homepage_with_redis(logged_id, look_at_user_id)
-    erb :personpage
+    "dame"
+    # erb :personpage
   end
 end
 
